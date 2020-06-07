939fc680062bead795f45e43fd8e80dbe8ef55fb
Tom Smeding <tom.smeding@gmail.com>
2020-06-07 13:52:55 +0200

    More random tests

--- test/tom/Main.hs
+++ test/tom/Main.hs
@@ -31,8 +31,28 @@ indexing = do
                                              in (store A.! A.index1 (2 * i)) + x)
                            (A.indexed source)
 
+apply :: IO ()
+apply = do
+  print $ I.run1 (A.map (*2)) (A.fromList (Z :. (10 :: Int)) [1.0::Float .. 10])
+  let prog = A.map (\x -> (x :: A.Exp Float) * 2)
+  print $ I.run1 prog (A.fromList (Z :. (10 :: Int)) [1.0::Float .. 10])
+  print $ prog
+  print $ I.run (let back = A.use (A.fromList (Z :. 5) [1.0::Float .. 5])
+                 in A.generate (A.index1 100)
+                               (\i -> back A.! A.index1 (A.unindex1 i `mod` A.length back)))
+
+ignoretest :: IO ()
+ignoretest = do
+  -- ignore == Z, so can't permute onto a scalar
+  print $ I.run
+    (A.permute (+) (A.use (A.fromList Z [0]))
+                   (\_ -> A.lift Z)
+                   (A.use (A.fromList (Z :. (10 :: Int)) [1::Float .. 10])))
+
 main :: IO ()
 main = do
-  logistic
-  optimise
-  indexing
+  -- logistic
+  -- optimise
+  -- indexing
+  -- apply
+  ignoretest
