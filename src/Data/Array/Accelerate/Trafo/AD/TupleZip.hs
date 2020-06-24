5cdb5b1dd2a6faf5f7765d9bb18c5af61a841bd8
Tom Smeding <tom.smeding@gmail.com>
2020-06-24 22:13:51 +0200

    Significantly simplify TupleZip
    Thanks @ivogabe

--- src/Data/Array/Accelerate/Trafo/AD/TupleZip.hs
+++ src/Data/Array/Accelerate/Trafo/AD/TupleZip.hs
@@ -6,66 +6,19 @@ module Data.Array.Accelerate.Trafo.AD.TupleZip (
 ) where
 
 import qualified Data.Array.Accelerate.AST as A
-import Data.Array.Accelerate.AST ((:>), (.>), (>:>), weakenWithLHS, weakenId, ELeftHandSide, LeftHandSide(..))
+import Data.Array.Accelerate.AST ((>:>), weakenWithLHS, weakenId, ELeftHandSide, LeftHandSide(..))
 import Data.Array.Accelerate.Type
+import Data.Array.Accelerate.Trafo.Base
 import Data.Array.Accelerate.Trafo.AD.Exp
 
 
 data GenLHS env t = forall env'. GenLHS (ELeftHandSide t env env')
-data TZDown lab t =
-    TZDown (forall env. GenLHS env t)
-           (forall env1 env1' env2 env2' env3.
-               ELeftHandSide t env1 env1'
-               -> env1' :> env2
-               -> ELeftHandSide t env2 env2'
-               -> env2' :> env3
-               -> Maybe (OpenExp env3 lab t))
 
-tzDown :: forall t lab.
-          (forall s env. ScalarType s -> OpenExp env lab s
-                                      -> OpenExp env lab s
-                                      -> Maybe (OpenExp env lab s))
-       -> TupleType t
-       -> TZDown lab t
-tzDown combine ty@(TupRpair t1 t2) =
-  case tzDown combine t1 of
-    TZDown genlhs1 resgen1 ->
-      case tzDown combine t2 of
-        TZDown genlhs2 resgen2 ->
-          TZDown
-            (case genlhs1 of
-               GenLHS genlhs1' ->
-                 case genlhs2 of
-                   GenLHS genlhs2' ->
-                     GenLHS (LeftHandSidePair genlhs1' genlhs2'))
-            (\lhs1 weak1 lhs2 weak2 ->
-                case lhs1 of
-                  LeftHandSidePair lhs11 lhs12 ->
-                    case lhs2 of
-                      LeftHandSidePair lhs21 lhs22 ->
-                        Pair ty
-                          <$> resgen1 lhs11 (weak1 .> weakenWithLHS lhs12) lhs21 (weak2 .> weakenWithLHS lhs22)
-                          <*> resgen2 lhs12 (weakenWithLHS lhs21 .> weak1) lhs22 weak2
-                      LeftHandSideSingle _ -> error "inconsistency in tzDown"
-                      LeftHandSideWildcard _ -> error "inconsistency in tzDown"
-                  LeftHandSideSingle _ -> error "inconsistency in tzDown"
-                  LeftHandSideWildcard _ -> error "inconsistency in tzDown")
-tzDown combine (TupRsingle sty) =
-  TZDown (GenLHS (LeftHandSideSingle sty))
-         (\lhs1 weak1 lhs2 weak2 ->
-             case lhs1 of
-               LeftHandSideSingle _ ->
-                 case lhs2 of
-                   LeftHandSideSingle _ ->
-                     combine sty
-                       (Var (A.Var sty (weak2 >:> SuccIdx (weak1 >:> ZeroIdx))))
-                       (Var (A.Var sty (weak2 >:> ZeroIdx)))
-                   LeftHandSidePair _ _ -> error "inconsistency in tzDown"
-                   LeftHandSideWildcard _ -> error "inconsistency in tzDown"
-               LeftHandSidePair _ _ -> error "inconsistency in tzDown"
-               LeftHandSideWildcard _ -> error "inconsistency in tzDown")
-tzDown _ TupRunit =
-  TZDown (GenLHS (LeftHandSideWildcard TupRunit)) (\_ _ _ _ -> Just Nil)
+type Combiner lab =
+  forall s env.
+    ScalarType s -> OpenExp env lab s
+                 -> OpenExp env lab s
+                 -> Maybe (OpenExp env lab s)
 
 sinkExp :: env A.:> env' -> OpenExp env lab t -> OpenExp env' lab t
 sinkExp _ (Const ty x) = Const ty x
@@ -88,18 +41,26 @@ generaliseLHS (LeftHandSidePair lhs1 lhs2) =
                 GenLHS lhs2' ->
                     GenLHS (LeftHandSidePair lhs1' lhs2')
 
+varsZip :: Combiner lab
+        -> TupleType t
+        -> A.ExpVars env t
+        -> A.ExpVars env t
+        -> Maybe (OpenExp env lab t)
+varsZip _ TupRunit A.VarsNil A.VarsNil =
+  Just Nil
+varsZip combine (TupRsingle ty) (A.VarsSingle v1) (A.VarsSingle v2) =
+  combine ty (Var v1) (Var v2)
+varsZip combine ty@(TupRpair t1 t2) (A.VarsPair v11 v12) (A.VarsPair v21 v22) =
+  Pair ty <$> varsZip combine t1 v11 v21 <*> varsZip combine t2 v12 v22
+varsZip _ _ _ _ = error "inconsistency in varsZip"
+
 tupleZip :: forall env lab t. TupleType t
-         -> (forall s env'. ScalarType s -> OpenExp env' lab s
-                                         -> OpenExp env' lab s
-                                         -> Maybe (OpenExp env' lab s))
+         -> Combiner lab
          -> OpenExp env lab t
          -> OpenExp env lab t
          -> Maybe (OpenExp env lab t)
-tupleZip ty combine e1 e2 =
-  case tzDown combine ty of
-    TZDown genlhs resgen ->
-      case genlhs of
-        GenLHS lhs1 ->
-          case genlhs of
-            GenLHS lhs2 ->
-              (Let lhs1 e1 . Let lhs2 (sinkExp (weakenWithLHS lhs1) e2)) <$> resgen lhs1 weakenId lhs2 weakenId
+tupleZip ty combine e1 e2
+  | DeclareVars lhs1 _ value1 <- declareVars ty
+  , DeclareVars lhs2 _ value2 <- declareVars ty
+  = Let lhs1 e1 . Let lhs2 (sinkExp (weakenWithLHS lhs1) e2)
+      <$> varsZip combine ty (value1 (weakenWithLHS lhs2)) (value2 weakenId)
