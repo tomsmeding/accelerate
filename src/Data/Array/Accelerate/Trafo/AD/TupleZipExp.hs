14b04cbfa2b3b3dd25370c1b6712d8a0e9b52749
Tom Smeding <tom.smeding@gmail.com>
2020-06-26 15:10:34 +0200

    Further simply TupleZip and friends

--- src/Data/Array/Accelerate/Trafo/AD/TupleZipExp.hs
+++ src/Data/Array/Accelerate/Trafo/AD/TupleZipExp.hs
@@ -16,31 +16,11 @@ import Data.Array.Accelerate.Trafo.Base
 import Data.Array.Accelerate.Type
 
 
-data GenLHS env t = forall env'. GenLHS (ELeftHandSide t env env')
-
 type Combiner lab =
   forall s env.
     ScalarType s -> OpenExp env lab s
                  -> OpenExp env lab s
                  -> Maybe (OpenExp env lab s)
-
-sinkExp :: env :> env' -> OpenExp env lab t -> OpenExp env' lab t
-sinkExp _ (Const ty x) = Const ty x
-sinkExp k (PrimApp op e) = PrimApp op (sinkExp k e)
-sinkExp k (Pair e1 e2) = Pair (sinkExp k e1) (sinkExp k e2)
-sinkExp _ Nil = Nil
-sinkExp k (Let lhs rhs e)
-  | GenLHS lhs' <- generaliseLHS lhs
-  = Let lhs' (sinkExp k rhs) (sinkExp (sinkWithLHS lhs lhs' k) e)
-sinkExp k (Evar (Var sty idx)) = Evar (Var sty (k >:> idx))
-
-generaliseLHS :: ELeftHandSide t env1 env1' -> GenLHS env2 t
-generaliseLHS (LeftHandSideWildcard ty) = GenLHS (LeftHandSideWildcard ty)
-generaliseLHS (LeftHandSideSingle ty) = GenLHS (LeftHandSideSingle ty)
-generaliseLHS (LeftHandSidePair lhs1 lhs2)
-  | GenLHS lhs1' <- generaliseLHS lhs1
-  , GenLHS lhs2' <- generaliseLHS lhs2
-  = GenLHS (LeftHandSidePair lhs1' lhs2')
 
 varsZip :: Combiner lab
         -> TupleType t
@@ -63,5 +43,5 @@ tupleZip :: forall env lab t. TupleType t
 tupleZip ty combine e1 e2
   | DeclareVars lhs1 _ value1 <- declareVars ty
   , DeclareVars lhs2 _ value2 <- declareVars ty
-  = Let lhs1 e1 . Let lhs2 (sinkExp (weakenWithLHS lhs1) e2)
+  = Let lhs1 e1 . Let lhs2 (weakenE (weakenWithLHS lhs1) e2)
       <$> varsZip combine ty (value1 (weakenWithLHS lhs2)) (value2 weakenId)
