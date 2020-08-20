8c6b67ff8797e552a282ecd0c75eee032f14ac3a
Tom Smeding <tom.smeding@gmail.com>
2020-08-20 16:08:17 +0200

    Everything is addable now NonNumType is removed

--- src/Data/Array/Accelerate/Trafo/AD/TupleZip.hs
+++ src/Data/Array/Accelerate/Trafo/AD/TupleZip.hs
@@ -2,8 +2,10 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeOperators #-}
 module Data.Array.Accelerate.Trafo.AD.TupleZip (
-  tupleZip
+  tupleZip, tupleZip'
 ) where
+
+import Data.Functor.Identity
 
 import qualified Data.Array.Accelerate.AST as A
 import Data.Array.Accelerate.AST.Environment (weakenWithLHS, weakenId)
@@ -14,33 +16,49 @@ import Data.Array.Accelerate.Trafo.AD.Sink
 import Data.Array.Accelerate.Trafo.Var
 
 
-type Combiner lab =
+type Combiner m lab =
   forall s env args.
     ScalarType s -> OpenExp env lab args s
                  -> OpenExp env lab args s
-                 -> Maybe (OpenExp env lab args s)
+                 -> m (OpenExp env lab args s)
 
-varsZip :: Combiner lab
+type Combiner' lab =
+  forall s env args.
+    ScalarType s -> OpenExp env lab args s
+                 -> OpenExp env lab args s
+                 -> OpenExp env lab args s
+
+varsZip :: Applicative m
+        => Combiner m lab
         -> TypeR t
         -> A.ExpVars env t
         -> A.ExpVars env t
-        -> Maybe (OpenExp env lab args t)
+        -> m (OpenExp env lab args t)
 varsZip _ TupRunit TupRunit TupRunit =
-  Just Nil
+  pure Nil
 varsZip combine (TupRsingle ty) (TupRsingle v1) (TupRsingle v2) =
   combine ty (Var v1) (Var v2)
 varsZip combine ty@(TupRpair t1 t2) (TupRpair v11 v12) (TupRpair v21 v22) =
   Pair ty <$> varsZip combine t1 v11 v21 <*> varsZip combine t2 v12 v22
 varsZip _ _ _ _ = error "inconsistency in varsZip"
 
-tupleZip :: forall env lab t args.
-            TypeR t
-         -> Combiner lab
+-- TODO: check whether this is actually used somewhere (and not only tupleZip')
+tupleZip :: Applicative m
+         => TypeR t
+         -> Combiner m lab
          -> OpenExp env lab args t
          -> OpenExp env lab args t
-         -> Maybe (OpenExp env lab args t)
+         -> m (OpenExp env lab args t)
 tupleZip ty combine e1 e2
   | DeclareVars lhs1 _ value1 <- declareVars ty
   , DeclareVars lhs2 _ value2 <- declareVars ty
   = Let lhs1 e1 . Let lhs2 (sinkExp (weakenWithLHS lhs1) e2)
       <$> varsZip combine ty (value1 (weakenWithLHS lhs2)) (value2 weakenId)
+
+tupleZip' :: TypeR t
+          -> Combiner' lab
+          -> OpenExp env lab args t
+          -> OpenExp env lab args t
+          -> OpenExp env lab args t
+tupleZip' ty combine' e1 e2 =
+  runIdentity $ tupleZip ty (\sty sub1 sub2 -> pure (combine' sty sub1 sub2)) e1 e2
