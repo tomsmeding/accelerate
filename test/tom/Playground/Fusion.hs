dfdf217f13dc6b627c8812fc6682e903dd444c53
Tom Smeding <tom.smeding@gmail.com>
2020-10-15 22:04:14 +0200

    Add example from issue #471 (shape prop)

--- test/tom/Playground/Fusion.hs
+++ test/tom/Playground/Fusion.hs
@@ -18,3 +18,12 @@ inputProgram =
        (zipWith (\x y -> T2 x y)
                 (map (\x -> x) arr)
                 (map (\x -> x) arr))
+
+shapeTest :: Acc (Matrix Float)
+shapeTest =
+  let a7 = use (fromList (Z :. (1 :: Int) :. (1 :: Int)) [5.0 :: Float])
+      a8 = map (\x0 -> T2 (x0 * x0) x0) a7
+      a9 = map (\(T2 x0 _) -> x0) a8
+  in zipWith (+)
+       (generate (shape a9) (\_ -> 1.0))
+       (map (\(T2 _ tup) -> tup) a8)
