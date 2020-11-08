1d286f681b99f67fe25dc08531cfeb6182fa4612
Tom Smeding <tom.smeding@gmail.com>
2020-11-08 10:50:55 +0100

    Make node/env-label distiction explicit in types

--- src/Data/Array/Accelerate/Trafo/AD/Common.hs
+++ src/Data/Array/Accelerate/Trafo/AD/Common.hs
@@ -1,5 +1,7 @@
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE QuantifiedConstraints #-}
 {-# LANGUAGE RankNTypes #-}
@@ -28,30 +30,54 @@ data TagVal s env where
     TEmpty :: TagVal s ()
     TPush :: TagVal s env -> s t -> TagVal s (env, t)
 
-data LabVal s lab env where
-    LEmpty :: LabVal s lab ()
-    LPush :: LabVal s lab env -> DLabel s lab t -> LabVal s lab (env, t)
+data LabVal lty s lab env where
+    LEmpty :: LabVal lty s lab ()
+    LPush :: LabVal lty s lab env -> DLabel lty s lab t -> LabVal lty s lab (env, t)
 
 data PartialVal s topenv env where
     PTEmpty :: PartialVal s topenv topenv
     PTPush :: PartialVal s topenv env -> s t -> PartialVal s topenv (env, t)
 
-data DLabel s lab t =
+-- This avoids a lot of -Wunticked-promoted-constructors.
+data LabelType = NodeLabel_ | EnvLabel_
+type NodeLabel = 'NodeLabel_
+type EnvLabel = 'EnvLabel_
+
+data DLabel (lty :: LabelType) s lab t =
     DLabel { labelType :: s t
            , labelLabel :: lab }
   deriving (Show)
 
-instance Show lab => GShow (DLabel TypeR      lab) where gshowsPrec = showsPrec
-instance Show lab => GShow (DLabel ScalarType lab) where gshowsPrec = showsPrec
-instance Show lab => GShow (DLabel ArraysR    lab) where gshowsPrec = showsPrec
-instance Show lab => GShow (DLabel ArrayR     lab) where gshowsPrec = showsPrec
+-- The type of a labelenv, which consists of single-typed environment labels.
+type ELabVal = LabVal EnvLabel ScalarType
+type ALabVal = LabVal EnvLabel ArrayR
 
-instance GEq s => GEq (DLabel s lab) where
+-- Normal environment labels, which are single-typed.
+type EDLabel = DLabel EnvLabel ScalarType
+type ADLabel = DLabel EnvLabel ArrayR
+
+-- Node labels are generally of tuple type, though sometimes we also need node
+-- labels in single-type form: explode deals with these due to destructuring
+-- let-bindings, and array labels embedded in expressions (to replace an array
+-- variable reference) are also of such type.
+type EDLabelN = DLabel NodeLabel TypeR
+type EDLabelNS = DLabel NodeLabel ScalarType
+type ADLabelN = DLabel NodeLabel ArraysR
+type ADLabelNS = DLabel NodeLabel ArrayR
+
+
+
+instance Show lab => GShow (DLabel lty TypeR      lab) where gshowsPrec = showsPrec
+instance Show lab => GShow (DLabel lty ScalarType lab) where gshowsPrec = showsPrec
+instance Show lab => GShow (DLabel lty ArraysR    lab) where gshowsPrec = showsPrec
+instance Show lab => GShow (DLabel lty ArrayR     lab) where gshowsPrec = showsPrec
+
+instance GEq s => GEq (DLabel lty s lab) where
     geq (DLabel ty1 _) (DLabel ty2 _) = do
         Refl <- geq ty1 ty2
         return Refl
 
-instance (Ord lab, GCompare s) => GCompare (DLabel s lab) where
+instance (Ord lab, GCompare s) => GCompare (DLabel lty s lab) where
     gcompare (DLabel ty1 lab1) (DLabel ty2 lab2) =
         case gcompare ty1 ty2 of
           GLT -> GLT
@@ -107,7 +133,7 @@ showTupR' f (TupRsingle ty) s = f ty s
 showTupR' f (TupRpair t1 t2) (a, b) =
     "(" ++ showTupR' f t1 a ++ ", " ++ showTupR' f t2 b ++ ")"
 
-showDLabel :: (Show (s t), Show lab) => DLabel s lab t -> String
+showDLabel :: (Show (s t), Show lab) => DLabel lty s lab t -> String
 showDLabel (DLabel ty lab) =
     let labshow = show lab
     in if all isDigit labshow
@@ -134,27 +160,27 @@ enumerateTupR TupRunit = []
 enumerateTupR (TupRsingle x) = [mkSome x]
 enumerateTupR (TupRpair t1 t2) = enumerateTupR t1 ++ enumerateTupR t2
 
-prjL :: Idx env t -> LabVal s lab env -> DLabel s lab t
+prjL :: Idx env t -> LabVal lty s lab env -> DLabel lty s lab t
 prjL ZeroIdx (LPush _ x) = x
 prjL (SuccIdx idx) (LPush env _) = prjL idx env
 
-mapLabVal :: (lab -> lab') -> LabVal s lab env -> LabVal s lab' env
+mapLabVal :: (lab -> lab') -> LabVal lty s lab env -> LabVal lty s lab' env
 mapLabVal _ LEmpty = LEmpty
 mapLabVal f (LPush env (DLabel ty lab)) = LPush (mapLabVal f env) (DLabel ty (f lab))
 
-labValContains :: Eq lab => LabVal s lab env -> lab -> Bool
+labValContains :: Eq lab => LabVal lty s lab env -> lab -> Bool
 labValContains LEmpty _ = False
 labValContains (LPush env (DLabel _ lab)) x =
     x == lab || labValContains env x
 
-uniqueLabVal :: Eq lab => LabVal s lab env -> Bool
+uniqueLabVal :: Eq lab => LabVal lty s lab env -> Bool
 uniqueLabVal LEmpty = True
 uniqueLabVal (LPush env (DLabel _ lab)) =
     not (labValContains env lab) && uniqueLabVal env
 
 data FoundTag s env = forall t. FoundTag (s t) (Idx env t)
 
-labValFind' :: Eq lab => LabVal s lab env -> lab -> Maybe (FoundTag s env)
+labValFind' :: Eq lab => LabVal lty s lab env -> lab -> Maybe (FoundTag s env)
 labValFind' LEmpty _ = Nothing
 labValFind' (LPush env (DLabel ty lab)) target
     | lab == target = Just (FoundTag ty ZeroIdx)
@@ -163,16 +189,14 @@ labValFind' (LPush env (DLabel ty lab)) target
             Just (FoundTag ty' idx) -> Just (FoundTag ty' (SuccIdx idx))
             Nothing -> Nothing
 
-tupleLabel :: DLabel s lab t -> DLabel (TupR s) lab t
+-- Only allowed for node labels, since env labels can only usefully refer to
+-- non-tuple values due to mandatory let-destructuring. This restriction is not
+-- syntactically necessary, but semantically it might prevent mishaps.
+tupleLabel :: DLabel NodeLabel s lab t -> DLabel NodeLabel (TupR s) lab t
 tupleLabel (DLabel ty lab) = DLabel (TupRsingle ty) lab
 
-fmapLabel :: (lab -> lab') -> DLabel s lab t -> DLabel s lab' t
+fmapLabel :: (lab -> lab') -> DLabel lty s lab t -> DLabel lty s lab' t
 fmapLabel f (DLabel ty lab) = DLabel ty (f lab)
-
-fmapLabels :: (lab -> lab') -> TupR (DLabel s lab) t -> TupR (DLabel s lab') t
-fmapLabels _ TupRunit = TupRunit
-fmapLabels f (TupRsingle lab) = TupRsingle (fmapLabel f lab)
-fmapLabels f (TupRpair labs1 labs2) = TupRpair (fmapLabels f labs1) (fmapLabels f labs2)
 
 pickTupR :: TupleIdx t t' -> TupR s t -> TupR s t'
 pickTupR TIHere tup = tup
@@ -198,10 +222,13 @@ newtype IdGen a = IdGen (State Int a)
 evalIdGen :: IdGen a -> a
 evalIdGen (IdGen s) = evalState s 1
 
-genId' :: s t -> IdGen (DLabel s Int t)
+genId' :: s t -> IdGen (DLabel lty s Int t)
 genId' ty = state (\s -> (DLabel ty s, succ s))
 
-lpushLabTup :: LabVal s lab env -> LeftHandSide s t env env' -> TupR (DLabel s lab) t -> LabVal s lab env'
+-- The restriction to env labels is not syntactically necessary, but
+-- semantically it generally doesn't make sense to push a tuple of node labels.
+-- Hence to prevent mishaps, this function is specialised to EnvLabel.
+lpushLabTup :: LabVal EnvLabel s lab env -> LeftHandSide s t env env' -> TupR (DLabel EnvLabel s lab) t -> LabVal EnvLabel s lab env'
 lpushLabTup labelenv (LeftHandSideWildcard _) TupRunit = labelenv
 lpushLabTup labelenv (LeftHandSideSingle _) (TupRsingle lab) = LPush labelenv lab
 lpushLabTup labelenv (LeftHandSidePair lhs1 lhs2) (TupRpair labs1 labs2) =
@@ -226,30 +253,33 @@ data PDAuxTagAcc = TmpTup
 type PDExp = PD PDAuxTagExp
 type PDAcc = PD PDAuxTagAcc
 
--- Expression node labels are of tuple type and have a PD tag.
--- Scalar value labels have no tag.
--- Since the Let bindings are on the scalar level (because Accelerate forces
---   tuple-destructuring), the labels in the environment are scalar labels.
---   These thus also have no tag.
+-- Expression node labels are of tuple type and have a PD tag; environment
+-- labels are of scalar type and have no tag.
+-- The labelenv recalls the environment of let-bound variables with environment
+-- labels; the bindmap maps node labels to the tuple of environment labels
+-- indicating where its value is stored.
 data Context s tag lab env =
-    Context (LabVal s lab env)
-            (DMap (DLabel (TupR s) (PD tag lab))
-                  (TupR (DLabel s lab)))
+    Context (LabVal EnvLabel s lab env)
+            (DMap (DLabel NodeLabel (TupR s) (PD tag lab))
+                  (TupR (DLabel EnvLabel s lab)))
+
+type EContext = Context ScalarType PDAuxTagExp
+type AContext = Context ArrayR PDAuxTagAcc
 
 showContext :: (Ord lab, Show lab, Show tag, Ord tag, forall t. Show (s t), forall t. Show (TupR s t))
             => Context s tag lab aenv -> String
 showContext (Context labelenv bindmap) = "Context " ++ showLabelenv labelenv ++ " " ++ showBindmap bindmap
 
-showLabelenv :: (Show lab, forall t. Show (s t)) => LabVal s lab aenv -> String
+showLabelenv :: (Show lab, forall t. Show (s t)) => LabVal lty s lab aenv -> String
 showLabelenv LEmpty = "[]"
 showLabelenv (LPush env lab) = "[" ++ go env ++ showDLabel lab ++ "]"
   where
-    go :: (Show lab, forall t. Show (s t)) => LabVal s lab aenv -> String
+    go :: (Show lab, forall t. Show (s t)) => LabVal lty s lab aenv -> String
     go LEmpty = ""
     go (LPush env' lab') = go env' ++ showDLabel lab' ++ ", "
 
 showBindmap :: (Ord lab, Show lab, Show tag, Ord tag, forall t. Show (s t), forall t. Show (TupR s t))
-            => DMap (DLabel (TupR s) (PD tag lab)) (TupR (DLabel s lab)) -> String
+            => DMap (DLabel NodeLabel (TupR s) (PD tag lab)) (TupR (DLabel EnvLabel s lab)) -> String
 showBindmap bindmap =
     let tups = sortOn fst [(lab, (showDLabel dlab, showTupR showDLabel labs))
                           | dlab@(DLabel _ lab) :=> labs <- DMap.assocs bindmap]
@@ -258,17 +288,23 @@ showBindmap bindmap =
     in "[" ++ s ++ "]"
 
 
--- TODO: make this 'type AnyLabel s lab = Exists (DLabel s lab)', and perhaps even inline this because then the typedef is marginally useful. Also apply this to other Any* names.
+-- TODO: make this 'type AnyLabel lty s lab = Exists (DLabel lty s lab)', and perhaps even inline this because then the typedef is marginally useful. Also apply this to other Any* names.
 -- (But consider the specialised Eq/Ord instances below. Can we reproduce that with an Exists version?)
-data AnyLabel s lab = forall t. AnyLabel (DLabel s lab t)
+data AnyLabel lty s lab = forall t. AnyLabel (DLabel lty s lab t)
 
-instance (Eq lab, GEq s) => Eq (AnyLabel s lab) where
+type EAnyLabel = AnyLabel EnvLabel ScalarType
+type EAnyLabelN = AnyLabel NodeLabel TypeR
+type AAnyLabel = AnyLabel EnvLabel ArrayR
+type AAnyLabelN = AnyLabel NodeLabel ArraysR
+type AAnyLabelNS = AnyLabel NodeLabel ArrayR
+
+instance (Eq lab, GEq s) => Eq (AnyLabel lty s lab) where
     AnyLabel (DLabel ty1 lab1) == AnyLabel (DLabel ty2 lab2)
       | lab1 /= lab2 = False
       | Just Refl <- geq ty1 ty2 = True
       | otherwise = error "Eq AnyLabel: labels match, but types do not!"
 
-instance (Ord lab, GCompare s) => Ord (AnyLabel s lab) where
+instance (Ord lab, GCompare s) => Ord (AnyLabel lty s lab) where
     compare (AnyLabel (DLabel ty1 lab1)) (AnyLabel (DLabel ty2 lab2)) =
         case compare lab1 lab2 of
           LT -> LT
