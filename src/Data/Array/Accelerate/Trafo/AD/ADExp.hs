008ce02217e0fb7d24eead7babccc32262b9480a
Tom Smeding <tom.smeding@gmail.com>
2020-11-10 18:00:03 +0100

    Use smartFst and smartSnd more

--- src/Data/Array/Accelerate/Trafo/AD/ADExp.hs
+++ src/Data/Array/Accelerate/Trafo/AD/ADExp.hs
@@ -1274,28 +1274,3 @@ dmapFind :: (HasCallStack, GCompare f) => DMap f g -> f a -> g a
 dmapFind mp elt = case DMap.lookup elt mp of
                     Just res -> res
                     Nothing -> error "dmapFind: not found"
-
--- TODO: make smartFst and smartSnd non-quadratic (should be easy)
-smartFst :: OpenExp env aenv lab alab args tenv (t1, t2) -> OpenExp env aenv lab alab args tenv t1
-smartFst (Pair _ ex _) = ex
-smartFst (Get (TupRpair t1 _) tidx ex) = Get t1 (insertFst tidx) ex
-  where insertFst :: TupleIdx t (t1, t2) -> TupleIdx t t1
-        insertFst TIHere = TILeft TIHere
-        insertFst (TILeft ti) = TILeft (insertFst ti)
-        insertFst (TIRight ti) = TIRight (insertFst ti)
-smartFst ex
-  | TupRpair t1 _ <- etypeOf ex
-  = Get t1 (TILeft TIHere) ex
-smartFst _ = error "smartFst: impossible GADTs"
-
-smartSnd :: OpenExp env aenv lab alab args tenv (t1, t2) -> OpenExp env aenv lab alab args tenv t2
-smartSnd (Pair _ _ ex) = ex
-smartSnd (Get (TupRpair _ t2) tidx ex) = Get t2 (insertSnd tidx) ex
-  where insertSnd :: TupleIdx t (t1, t2) -> TupleIdx t t2
-        insertSnd TIHere = TIRight TIHere
-        insertSnd (TILeft ti) = TILeft (insertSnd ti)
-        insertSnd (TIRight ti) = TIRight (insertSnd ti)
-smartSnd ex
-  | TupRpair _ t2 <- etypeOf ex
-  = Get t2 (TIRight TIHere) ex
-smartSnd _ = error "smartSnd: impossible GADTs"
