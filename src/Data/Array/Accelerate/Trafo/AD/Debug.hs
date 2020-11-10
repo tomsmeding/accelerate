b3ce91f9ce8c6b3824054993d55c53ad9796a110
Tom Smeding <tom.smeding@gmail.com>
2020-11-10 18:07:19 +0100

    Enable code to know whether debug tracing is enabled

--- src/Data/Array/Accelerate/Trafo/AD/Debug.hs
+++ src/Data/Array/Accelerate/Trafo/AD/Debug.hs
@@ -11,14 +11,21 @@ import Data.Array.Accelerate.Trafo.AD.Config
 
 trace :: String -> a -> a
 #ifdef DEBUG
-trace s = if getConfigVar Debug then Trace.trace s else id
+trace s = if adTraceEnabled then Trace.trace s else id
 #else
 trace _ = id
 #endif
 
 traceM :: Monad m => String -> m ()
 #ifdef DEBUG
-traceM = if getConfigVar Debug then Trace.traceM else const (return ())
+traceM = if adTraceEnabled then Trace.traceM else const (return ())
 #else
 traceM _ = return ()
+#endif
+
+adTraceEnabled :: Bool
+#ifdef DEBUG
+adTraceEnabled = getConfigVar Debug
+#else
+adTraceEnabled = False
 #endif
